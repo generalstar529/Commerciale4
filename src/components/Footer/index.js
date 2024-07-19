9ce4c34735c00d38cfad1c8d0908b86737c15bde
generalstar529 <generalstar529@gmail.com>
2024-07-19 04:11:17 -0400

    Convert Footer component from class to hooks

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,25 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import "./index.css";
 import { STRINGS } from "../../utils/strings";
 
-export default class Footer extends Component {
-    render() {
-        return (
-            <div className="footer row">
-                <div className="col-md-4">
-                    <a className="normal" href="/terms">
-                        {STRINGS.terms}
-                    </a>
-                </div>
-                <div className="col-md-4">
-                    <a className="normal" href="policy">
-                        {STRINGS.policy}
-                    </a>
-                </div>
-                <div className="col-md-4">
-                    {STRINGS.contactUs}: <a href="mailto:info@commerciale.it">info@commerciale40.it</a>
-                </div>
+const Footer = () => {
+    return (
+        <div className="footer row">
+            <div className="col-md-4">
+                <a className="normal" href="/terms">
+                    {STRINGS.terms}
+                </a>
             </div>
-        );
-    }
+            <div className="col-md-4">
+                <a className="normal" href="policy">
+                    {STRINGS.policy}
+                </a>
+            </div>
+            <div className="col-md-4">
+                {STRINGS.contactUs}: <a href="mailto:info@commerciale.it">info@commerciale40.it</a>
+            </div>
+        </div>
+    );
 }
+
+export default Footer;
