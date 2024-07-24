88075b6fbedd41fa0d224da08093c44b8211b4c3
generalstar529 <generalstar529@gmail.com>
2024-07-24 12:46:10 -0400

    Convert ResetPassword page from class to hooks

--- src/pages/ResetPassword/index.js
+++ src/pages/ResetPassword/index.js
@@ -1,82 +1,75 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./index.css";
 import * as Validate from "../../utils/Validate";
 import { Alert } from "react-bootstrap";
 import { requestAPI } from "../../utils/api";
 import { STRINGS } from "../../utils/strings";
 
-export default class ResetPasswordPage extends Component {
-    constructor(props) {
-        super(props);
+const ResetPasswordPage = () => {
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [alertText, setAlertText] = useState(null);
 
-        this.state = {
-            isSuccess: false,
-            alertText: null,
-        };
+    const refPassword = React.createRef();
+    const refConfirm = React.createRef();
 
-        this.refPassword = React.createRef();
-        this.refConfirm = React.createRef();
-    }
-
-    handleClickSave = async (e) => {
-        let valid = Validate.checkPassword(this.refPassword.current.value);
-        Validate.applyToInput(this.refPassword.current, valid.code);
+    const handleClickSave = async (e) => {
+        let valid = Validate.checkPassword(refPassword.current.value);
+        Validate.applyToInput(refPassword.current, valid.code);
         if (valid.code !== Validate.VALID) {
-            this.setState({ alertText: [{ langKey: "password" }, { validCode: valid.code }] });
+            setAlertText([{ langKey: "password" }, { validCode: valid.code }]);
             return;
         }
 
-        valid = Validate.checkConfirmPassword(this.refPassword.current.value, this.refConfirm.current.value);
+        valid = Validate.checkConfirmPassword(refPassword.current.value, refConfirm.current.value);
 
-        Validate.applyToInput(this.refConfirm.current, valid.code);
+        Validate.applyToInput(refConfirm.current, valid.code);
         if (valid.code !== Validate.VALID) {
-            this.setState({ alertText: [{ validCode: valid.code }] });
+            setAlertText([{ validCode: valid.code }])
             return;
         }
 
-        this.setState({ isProcessing: true });
-        let response = await requestAPI(`/companies/${this.props.match.params.id}/password`, "POST", { password: this.refPassword.current.value });
+        let response = await requestAPI(`/companies/${this.props.match.params.id}/password`, "POST", { password: refPassword.current.value });
         let result = await response.json();
-        this.setState({ isProcessing: false });
         if (result.error) {
-            this.setState({ alertText: [{ langKey: result.error }] });
+            setAlertText([{ langKey: result.error }]);
             return;
         }
 
-        this.setState({ isSuccess: true, alertText: [{ langKey: "passwordReset" }] });
+        setIsSuccess(true);
+        setAlertText([{ langKey: "passwordReset" }]);
         setTimeout(() => {
             window.location.href = "/";
         }, 1000);
     };
 
-    render() {
-        const { isSuccess, alertText } = this.state;
-        const successDiv = (
+    const successDiv = (
+        <div>
+            <Alert variant="success">{Validate.getAlertMsg(alertText)}</Alert>
+        </div>
+    );
+    const failDiv = (
+        <div>
+            {alertText ? <Alert variant="danger">{Validate.getAlertMsg(alertText)}</Alert> : <div></div>}
             <div>
-                <Alert variant="success">{Validate.getAlertMsg(alertText)}</Alert>
-            </div>
-        );
-        const failDiv = (
-            <div>
-                {alertText ? <Alert variant="danger">{Validate.getAlertMsg(alertText)}</Alert> : <div></div>}
-                <div>
-                    <div className="text-center">
-                        <i className="fa fa-lock" />
-                    </div>
-                    <div className="title text-center">{STRINGS.resetYourPassword}</div>
-                    <div className="text">{STRINGS.createPassword}</div>
-                    <div className="my-3 d-flex justify-content-center">
-                        <input type="password" placeholder={STRINGS.newPassword} ref={this.refPassword} />
-                    </div>
-                    <div className="my-3 d-flex justify-content-center">
-                        <input type="password" placeholder={STRINGS.confirmPassword} ref={this.refConfirm} />
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        <button onClick={this.handleClickSave.bind(this)}>{STRINGS.save}</button>
-                    </div>
+                <div className="text-center">
+                    <i className="fa fa-lock" />
+                </div>
+                <div className="title text-center">{STRINGS.resetYourPassword}</div>
+                <div className="text">{STRINGS.createPassword}</div>
+                <div className="my-3 d-flex justify-content-center">
+                    <input type="password" placeholder={STRINGS.newPassword} ref={refPassword} />
+                </div>
+                <div className="my-3 d-flex justify-content-center">
+                    <input type="password" placeholder={STRINGS.confirmPassword} ref={refConfirm} />
+                </div>
+                <div className="d-flex justify-content-center">
+                    <button onClick={handleClickSave.bind(this)}>{STRINGS.save}</button>
                 </div>
             </div>
-        );
-        return <div className="reset-password">{isSuccess ? successDiv : failDiv}</div>;
-    }
+        </div>
+    );
+    
+    return <div className="reset-password">{isSuccess ? successDiv : failDiv}</div>;
 }
+
+export default ResetPasswordPage;
