2543a52f97c49e799a4421e6334480f7aa480028
generalstar529 <generalstar529@gmail.com>
2024-07-26 07:48:51 -0400

    Convert ForgotPassword page from class to hooks

--- src/pages/ForgotPassword/index.js
+++ src/pages/ForgotPassword/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./index.css";
 import * as Validate from "../../utils/Validate";
 import { Alert } from "react-bootstrap";
@@ -6,57 +6,48 @@ import { requestAPI } from "../../utils/api";
 import { STRINGS } from "../../utils/strings";
 import SpinnerView from "../../components/SpinnerView";
 
-export default class ForgotPasswordPage extends Component {
-    constructor(props) {
-        super(props);
+const ForgotPasswordPage = () => {
+    const [alertData, setAlertData] = useState(null);
+    const [isProgressing, setIsProgressing] = useState(false);
 
-        this.state = {
-            alertData: null,
-            isProgressing: false,
-        };
+    const refEmail = React.createRef();
 
-        this.refEmail = React.createRef();
-    }
-
-    handleClickDone = async (e) => {
-        let valid = Validate.checkEmail(this.refEmail.current.value);
-        Validate.applyToInput(this.refEmail.current, valid.code);
+    const handleClickDone = async (e) => {
+        let valid = Validate.checkEmail(refEmail.current.value);
+        Validate.applyToInput(refEmail.current, valid.code);
         if (valid.code !== Validate.VALID) {
-            this.setState({
-                alertData: { variant: "danger", messages: [{ langKey: "emailAddress" }, { validCode: valid.code }] },
-            });
+            setAlertData({ variant: "danger", messages: [{ langKey: "emailAddress" }, { validCode: valid.code }] });
             return false;
         }
 
-        this.setState({ isProgressing: true });
-        let response = await requestAPI(`/companies/auth/forgot-password/${this.refEmail.current.value}`, "GET");
+        setIsProgressing(true);
+        let response = await requestAPI(`/companies/auth/forgot-password/${refEmail.current.value}`, "GET");
         let result = await response.json();
-        this.setState({ isProgressing: false });
+        setIsProgressing(false);
         if (result.error) {
-            this.setState({ alertData: { variant: "danger", messages: [{ langKey: result.error }] } });
+            setAlertData({ variant: "danger", messages: [{ langKey: result.error }] });
             return;
         }
-        this.setState({ alertData: { variant: "success", messages: [{ langKey: "sentResetPassword" }] } });
+        setAlertData({ variant: "success", messages: [{ langKey: "sentResetPassword" }] });
     };
 
-    render() {
-        const { alertData, isProcessing } = this.state;
-        return (
-            <div className="forgot-password">
-                {alertData ? <Alert variant={alertData.variant}>{Validate.getAlertMsg(alertData.messages)}</Alert> : <div></div>}
-                <div className="text-center">
-                    <i className="fa fa-lock" />
-                </div>
-                <div className="title text-center">{STRINGS.forgotPassword}</div>
-                <div className="text">{STRINGS.forgotPasswordMsg}</div>
-                <div className="my-3 d-flex justify-content-center">
-                    <input type="text" name="email" placeholder="Email" ref={this.refEmail} />
-                </div>
-                <div className="d-flex justify-content-center">
-                    <button onClick={this.handleClickDone.bind(this)}>{STRINGS.sendEmail}</button>
-                </div>
-                {isProcessing && <SpinnerView />}
+    return (
+        <div className="forgot-password">
+            {alertData ? <Alert variant={alertData.variant}>{Validate.getAlertMsg(alertData.messages)}</Alert> : <div></div>}
+            <div className="text-center">
+                <i className="fa fa-lock" />
             </div>
-        );
-    }
+            <div className="title text-center">{STRINGS.forgotPassword}</div>
+            <div className="text">{STRINGS.forgotPasswordMsg}</div>
+            <div className="my-3 d-flex justify-content-center">
+                <input type="text" name="email" placeholder="Email" ref={refEmail} />
+            </div>
+            <div className="d-flex justify-content-center">
+                <button onClick={handleClickDone.bind(this)}>{STRINGS.sendEmail}</button>
+            </div>
+            {isProgressing && <SpinnerView />}
+        </div>
+    );
 }
+
+export default ForgotPasswordPage;
