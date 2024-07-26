06a5c55f776f81ea0de5ca306c711f5e802803bc
generalstar529 <generalstar529@gmail.com>
2024-07-26 08:55:07 -0400

    Convert Landing page from class to hooks

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import "./index.css";
 import LoginForm from "../../components/LoginForm";
 import FilterForm from "../../components/FilterForm";
@@ -7,64 +7,65 @@ import { SESSION_LOGGED_COMPANY } from "../../utils";
 import { STRINGS } from "../../utils/strings";
 import { requestAPI } from "../../utils/api";
 
-export default class LandingPage extends Component {
-    componentDidMount = async () => {
+const LandingPage = () => {
+    useEffect(async () => {
         await requestAPI("/init", "GET");
-    };
+    }, []);
 
-    handleClickSearch = (filter) => {
-        sessionStorage.setItem("filter", JSON.stringify(filter));
-        window.location.href = "/dashboard";
-    };
-
-    handleKeySearch = (key) => {
+    const handleKeySearch = (key) => {
         sessionStorage.setItem("filter", JSON.stringify({ key: key }));
         window.location.href = "/dashboard";
     };
 
-    render() {
-        const loggedUser = JSON.parse(sessionStorage.getItem(SESSION_LOGGED_COMPANY));
-        return (
-            <div className="landing">
-                <div className="slide">
-                    <div className={`login-panel ${loggedUser ? "logged" : ""}`}>
-                        <div>
-                            <div className="my-form intro">
-                                <span>COMMERCIALE4.0.COM</span> {STRINGS.intro1}
-                                <p>{STRINGS.joinUs}</p>
-                            </div>
-                            <LoginForm />
+    const handleClickSearch = (filter) => {
+        sessionStorage.setItem("filter", JSON.stringify(filter));
+        window.location.href = "/dashboard";
+    };
+
+    const loggedUser = JSON.parse(sessionStorage.getItem(SESSION_LOGGED_COMPANY));
+
+    return (
+        <div className="landing">
+            <div className="slide">
+                <div className={`login-panel ${loggedUser ? "logged" : ""}`}>
+                    <div>
+                        <div className="my-form intro">
+                            <span>COMMERCIALE4.0.COM</span> {STRINGS.intro1}
+                            <p>{STRINGS.joinUs}</p>
                         </div>
-                    </div>
-                    <div className={`search-panel ${loggedUser ? "logged" : ""}`}>
-                        {/* <div className="search-bar">
-							<SearchInput handleSearch={this.handleKeySearch} />
-						</div> */}
-                        <FilterForm handleSearch={this.handleClickSearch} />
+                        <LoginForm />
                     </div>
                 </div>
-                <div className="about">
-                    <div className="d-flex py-3">
-                        <div>
-                            <img src="images/profile.png" alt="" />
-                        </div>
-                        <div className="pl-4 pt-3">
-                            <p className="title">{STRINGS.companyProfile}</p>
-                            <p>{STRINGS.createCompanyProfile}</p>
-                        </div>
+                <div className={`search-panel ${loggedUser ? "logged" : ""}`}>
+                    {/* <div className="search-bar">
+                        <SearchInput handleSearch={this.handleKeySearch} />
+                    </div> */}
+                    <FilterForm handleSearch={handleClickSearch} />
+                </div>
+            </div>
+            <div className="about">
+                <div className="d-flex py-3">
+                    <div>
+                        <img src="images/profile.png" alt="" />
                     </div>
-                    <hr />
-                    <div className="d-flex py-3">
-                        <div>
-                            <img src="images/earth.png" alt="" />
-                        </div>
-                        <div className="pl-4 pt-3">
-                            <p className="title">{STRINGS.advancedSearchSystem}</p>
-                            <p>{STRINGS.makeDetailResearch}</p>
-                        </div>
+                    <div className="pl-4 pt-3">
+                        <p className="title">{STRINGS.companyProfile}</p>
+                        <p>{STRINGS.createCompanyProfile}</p>
+                    </div>
+                </div>
+                <hr />
+                <div className="d-flex py-3">
+                    <div>
+                        <img src="images/earth.png" alt="" />
+                    </div>
+                    <div className="pl-4 pt-3">
+                        <p className="title">{STRINGS.advancedSearchSystem}</p>
+                        <p>{STRINGS.makeDetailResearch}</p>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+export default LandingPage;
