4c8679cfc751950304bb9cb03f2a473d5b9bdba9
generalstar529 <generalstar529@gmail.com>
2024-08-05 10:53:23 -0400

    Convert some pages from class to hooks

--- src/App.js
+++ src/App.js
@@ -22,73 +22,84 @@ import { SESSION_LANG } from "./utils";
 import ReactGA from "react-ga";
 
 const App = () => {
-    const [headerTransparent, setHeaderTransparent] = useState(false);
-    const [headerAutoHide, setHeaderAutoHide] = useState(true);
-    const [selectedLang, setSelectedLang] = useState(sessionStorage.getItem(SESSION_LANG));
-    const [needSearchBar, setNeedSearchBar] = useState(true);
-    const [needFooter, setNeedFooter] = useState(true);
+  const [headerTransparent, setHeaderTransparent] = useState(false);
+  const [headerAutoHide, setHeaderAutoHide] = useState(true);
+  const [selectedLang, setSelectedLang] = useState(
+    sessionStorage.getItem(SESSION_LANG)
+  );
+  const [needSearchBar, setNeedSearchBar] = useState(true);
+  const [needFooter, setNeedFooter] = useState(true);
 
-    const setLocalization = () => {
-        let lang = sessionStorage.getItem(SESSION_LANG);
-        if (!lang) {
-            sessionStorage.setItem(SESSION_LANG, "it");
-        }
-        STRINGS.setLanguage(lang ? lang : "it");
+  const setLocalization = () => {
+    let lang = sessionStorage.getItem(SESSION_LANG);
+    if (!lang) {
+      sessionStorage.setItem(SESSION_LANG, "it");
     }
+    STRINGS.setLanguage(lang ? lang : "it");
+  };
 
-    useEffect(() => {
-        ReactGA.initialize("UA-167422154-1");
-        ReactGA.pageview(window.location.pathname);
+  useEffect(() => {
+    ReactGA.initialize("UA-167422154-1");
+    ReactGA.pageview(window.location.pathname);
 
-        if (window.location.pathname === "/") {
-            setHeaderTransparent(true);
-        }
-        if (window.location.pathname.search("/company/") !== -1) {
-            setHeaderAutoHide(false);
-            setNeedFooter(false);
-        }
-        if (window.location.pathname === "/user-edit") {
-            setNeedFooter(false);
-        }
-        if (window.location.pathname === "/login" || window.location.pathname === "/register" || window.location.pathname === "/forgot-password") {
-            setNeedSearchBar(false);
-            setNeedFooter(false);
-            setHeaderTransparent(true);
-        }
-        setLocalization();
-    }, [])
+    if (window.location.pathname === "/") {
+      setHeaderTransparent(true);
+    }
+    if (window.location.pathname.search("/company/") !== -1) {
+      setHeaderAutoHide(false);
+      setNeedFooter(false);
+    }
+    if (window.location.pathname === "/user-edit") {
+      setNeedFooter(false);
+    }
+    if (
+      window.location.pathname === "/login" ||
+      window.location.pathname === "/register" ||
+      window.location.pathname === "/forgot-password"
+    ) {
+      setNeedSearchBar(false);
+      setNeedFooter(false);
+      setHeaderTransparent(true);
+    }
+    setLocalization();
+  }, []);
 
-    const selectLang = (lang) => {
-        setSelectedLang(lang);
-        sessionStorage.setItem(SESSION_LANG, lang);
-        setLocalization();
-    };
+  const selectLang = (lang) => {
+    setSelectedLang(lang);
+    sessionStorage.setItem(SESSION_LANG, lang);
+    setLocalization();
+  };
 
-    return (
-        <div>
-            <LangProvider value={{ lang: selectedLang }}>
-                <Router>
-                    <Header needSearchBar={needSearchBar} isTransparent={headerTransparent} autoHide={headerAutoHide} onSelectedLang={selectLang} />
-                    <div className="body">
-                        <Switch>
-                            <Route exact path="/" component={LandingPage} />
-                            <Route exact path="/login" component={LoginPage} />
-                            <Route exact path="/register" component={RegisterPage} />
-                            <Route path="/forgot-password" component={ForgotPasswordPage} />
-                            <Route path="/reset-password/:id" component={ResetPasswordPage} />
-                            <Route exact path="/terms" component={TermsAndConditions} />
-                            <Route exact path="/policy" component={PrivacyPolicy} />
-                            <Route exact path="/dashboard" component={Dashboard} />
-                            <Route exact path="/company/:id" component={CompanyDetail} />
-                            <Route exact path="/user-edit" component={Profile} />
-                            <Route component={Error404} />
-                        </Switch>
-                    </div>
-                    {needFooter && <Footer />}
-                </Router>
-            </LangProvider>
-        </div>
-    );
-}
+  return (
+    <div>
+      <LangProvider value={{ lang: selectedLang }}>
+        <Router>
+          <Header
+            needSearchBar={needSearchBar}
+            isTransparent={headerTransparent}
+            autoHide={headerAutoHide}
+            onSelectedLang={selectLang}
+          />
+          <div className="body">
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route exact path="/login" component={LoginPage} />
+              <Route exact path="/register" component={RegisterPage} />
+              <Route path="/forgot-password" component={ForgotPasswordPage} />
+              <Route path="/reset-password/:id" component={ResetPasswordPage} />
+              <Route exact path="/terms" component={TermsAndConditions} />
+              <Route exact path="/policy" component={PrivacyPolicy} />
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/company/:id" component={CompanyDetail} />
+              <Route exact path="/user-edit" component={Profile} />
+              <Route component={Error404} />
+            </Switch>
+          </div>
+          {needFooter && <Footer />}
+        </Router>
+      </LangProvider>
+    </div>
+  );
+};
 
 export default App;
--- src/pages/Error404/index.js
+++ src/pages/Error404/index.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import "./index.css";
 import { STRINGS } from "../../utils/strings";
 
-export default class Error404 extends Component {
-    render() {
-        return (
-            <div className="except d-flex align-items-center justify-content-center">
-                <div className="except-content text-center p-5">
-                    <p className="title">404</p>
-                    <p className="error">{STRINGS.pageNotFound}</p>
-                    <hr className="my-5" />
-                    <p className="check mb-3">{STRINGS.checkUrl}</p>
-                    <p className="click">
-                        {STRINGS.otherwise}, <a href="/">{STRINGS.clickHere}</a> {STRINGS.redirectHome}
-                    </p>
-                </div>
-            </div>
-        );
-    }
-}
+const Error404 = () => {
+  return (
+    <div className="except d-flex align-items-center justify-content-center">
+      <div className="except-content text-center p-5">
+        <p className="title">404</p>
+        <p className="error">{STRINGS.pageNotFound}</p>
+        <hr className="my-5" />
+        <p className="check mb-3">{STRINGS.checkUrl}</p>
+        <p className="click">
+          {STRINGS.otherwise}, <a href="/">{STRINGS.clickHere}</a>{" "}
+          {STRINGS.redirectHome}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Error404;
