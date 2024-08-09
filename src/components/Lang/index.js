7e9f9154f6a2dccc544bee5b505e707de09768b0
generalstar529 <generalstar529@gmail.com>
2024-08-09 07:38:46 -0700

    Convert Lang component from class to hooks

--- src/components/Lang/index.js
+++ src/components/Lang/index.js
@@ -1,45 +1,40 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./index.css";
 import { SESSION_LANG } from "../../utils";
 
 const LANGUAGES = [
-    {
-        value: "it",
-        imgUrl: "/images/flag/italy1.png",
-    },
-    {
-        value: "en",
-        imgUrl: "/images/flag/uk1.png",
-    },
+  {
+    value: "it",
+    imgUrl: "/images/flag/italy1.png",
+  },
+  {
+    value: "en",
+    imgUrl: "/images/flag/uk1.png",
+  },
 ];
 
-export default class Lang extends Component {
-    constructor(props) {
-        super(props);
+export const Lang = ({ onChange }) => {
+  const lang = sessionStorage.getItem(SESSION_LANG);
+  const [selectedLang, setSelectedLang] = useState(lang ? lang : "en");
 
-        let lang = sessionStorage.getItem(SESSION_LANG);
-        this.state = {
-            selectedLang: lang ? lang : "en",
-        };
+  const handleChangeLang = (_selectedLang) => {
+    setSelectedLang(_selectedLang);
+    if (onChange) {
+      onChange(_selectedLang);
     }
+  };
 
-    handleChangeLang = (selectedLang) => {
-        const { onChange } = this.props;
-
-        this.setState({ selectedLang });
-        if (onChange) {
-            onChange(selectedLang);
-        }
-    };
-
-    render() {
-        const { selectedLang } = this.state;
-        return (
-            <div className="lang d-flex">
-                {LANGUAGES.map((lang, index) => (
-                    <img key={index} className={selectedLang === lang.value ? "active" : ""} src={lang.imgUrl} alt="" onClick={() => this.handleChangeLang(lang.value)} />
-                ))}
-            </div>
-        );
-    }
-}
+  return (
+    <div className="lang d-flex">
+      {LANGUAGES.map((lang, index) => (
+        <img
+          key={index}
+          className={selectedLang === lang.value ? "active" : ""}
+          src={lang.imgUrl}
+          alt=""
+          onClick={() => handleChangeLang(lang.value)}
+        />
+      ))}
+    </div>
+  );
+};
