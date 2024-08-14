f01d3bece538844c704982dc37826a91fd12ec47
generalstar529 <generalstar529@gmail.com>
2024-08-14 06:09:30 -0700

    Convert Sidebar component from class to hooks

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,43 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import "./index.css";
 
-export default class Sidebar extends Component {
-    state = {
-        background: "#eee",
-        offsetTop: 46,
-        contentWidth: 50,
-    };
+export const Sidebar = (props) => {
+  const state = {
+    background: "#eee",
+    offsetTop: 46,
+    contentWidth: 50,
+  };
 
-    render() {
-        let offsetTop = this.props.offsetTop ? this.props.offsetTop : this.state.offsetTop;
-        let contentWidth = this.props.contentWidth ? this.props.contentWidth : this.state.contentWidth;
-        let background = this.props.background ? this.props.background : this.state.background;
-        return (
-            <div
-                className={`sidebar ${this.props.isExpanded ? "expanded" : "normal"}`}
-                style={{
-                    top: offsetTop,
-                    height: `calc(100vh - ${offsetTop}px)`,
-                }}
-            >
-                <div
-                    style={{
-                        width: `${contentWidth}%`,
-                        background: background,
-                    }}
-                >
-                    {this.props.children}
-                </div>
-                <div style={{ width: `${100 - contentWidth}%` }} onClick={this.props.handleCollapse}>
-                    {this.props.isCloseButton ? (
-                        <button className="secondary btn-close">
-                            <i className="fa fa-close" />
-                        </button>
-                    ) : (
-                        <div />
-                    )}
-                </div>
-            </div>
-        );
-    }
-}
+  const offsetTop = props.offsetTop ? props.offsetTop : state.offsetTop;
+  const contentWidth = props.contentWidth
+    ? props.contentWidth
+    : state.contentWidth;
+  const background = props.background ? props.background : state.background;
+
+  return (
+    <div
+      className={`sidebar ${props.isExpanded ? "expanded" : "normal"}`}
+      style={{
+        top: offsetTop,
+        height: `calc(100vh - ${offsetTop}px)`,
+      }}
+    >
+      <div
+        style={{
+          width: `${contentWidth}%`,
+          background: background,
+        }}
+      >
+        {props.children}
+      </div>
+      <div
+        style={{ width: `${100 - contentWidth}%` }}
+        onClick={props.handleCollapse}
+      >
+        {props.isCloseButton ? (
+          <button className="secondary btn-close">
+            <i className="fa fa-close" />
+          </button>
+        ) : (
+          <div />
+        )}
+      </div>
+    </div>
+  );
+};
